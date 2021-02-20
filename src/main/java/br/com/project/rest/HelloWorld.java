5bdcfac3cd7be27d2272832f024ae5c614c8ac37
Phguima <pedrinho_360@hotmail.com>
2021-02-19 21:06:59 -0300

    Uploading project to the repository

---
+++ src/main/java/br/com/project/rest/HelloWorld.java
@@ -0,0 +1,18 @@
+package br.com.project.rest;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
+		System.out.println(response.getBody().asString().equals("Ola Mundo!"));
+		System.out.println(response.statusCode() == 200);
+		
+		ValidatableResponse validate = response.then();
+		validate.statusCode(200);
+	}
+}
