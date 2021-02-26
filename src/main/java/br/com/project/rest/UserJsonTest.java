b60a546956a49cb57385bd06c39d121b81284534
Phguima <pedrinho_360@hotmail.com>
2021-02-26 00:35:37 -0300

    Validações avançadas usando Groovy

--- src/main/java/br/com/project/rest/UserJsonTest.java
+++ src/main/java/br/com/project/rest/UserJsonTest.java
@@ -3,6 +3,8 @@ package br.com.project.rest;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+import java.util.Arrays;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -78,5 +80,37 @@ public class UserJsonTest {
 		.then()
 			.statusCode(STATUS_404)
 			.body("error", is("Usuário inexistente"));
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void verificarListaRaiz() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/users")
+		.then()
+			.statusCode(STATUS_200)
+			.body("name", hasItems("João da Silva", "Maria Joaquina", "Ana Júlia"))
+			.body("age[1]", is(25))
+			.body("filhos.name", hasItem(Arrays.asList("Zezinho", "Luizinho")))
+			.body("salary", contains(1234.5678f, 2500, null))
+			.body("$", hasSize(3));
+	}
+	
+	@Test
+	public void verificacaoAvancada() {
+		given()
+		.when()
+			.get("http://restapi.wcaquino.me/users")
+		.then()
+			.statusCode(STATUS_200)
+			.body("$", hasSize(3))
+			.body("age.findAll{it <= 25}.size()", is(2))
+			.body("age.findAll{it <= 25 && it > 20}.size()", is(1))
+			.body("findAll{it.age <= 25 && it.age > 20}.name", hasItem("Maria Joaquina"))
+			.body("findAll{it.age <= 25}[0].name", is("Maria Joaquina")) //Transforma a Lista em um Objeto
+			.body("findAll{it.age <= 25}[-1].name", is("Ana Júlia")) //Pega do último para o primeiro
+			.body("find{it.age <= 25}.name", is("Maria Joaquina")) //Find retorna apenas um
+			.body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina", "Ana Júlia"));
 	}
 }
